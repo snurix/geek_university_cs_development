2b4797d3e194780663e892a01306d5e801c1aaec
snurix <snurix@gmail.com>
2021-03-02 11:57:20 +0300

    add lesson 3

---
+++ lesson_3/lesson_3_2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson_3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] arrBook = new string[5, 2] 
+            { 
+                { "Петров А.А.\t", "petrov@mail.ru" }, { "Иванов А. А.\t", "d@mail.ru" }, { "Сидоров А. А.\t", "d@mail.ru" }, {"Васильев А. А.\t", "a@mail.ru"}, {"Смирнов А. А.\t", "s@mail.ru"} 
+            };
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.Write($"{arrBook[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ lesson_3/lesson_3_3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson_3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Введите любые символы: ");
+            string s = Console.ReadLine();
+            char[] sReverse = s.ToCharArray();
+            Array.Reverse(sReverse);
+            s = new string(sReverse);
+            Console.WriteLine(s);
+            Console.ReadLine();
+
+        }
+    }
+}
